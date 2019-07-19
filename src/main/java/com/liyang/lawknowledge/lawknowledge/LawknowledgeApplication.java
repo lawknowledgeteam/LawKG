eca252f4f1e89be108db956b2df5e31f52c469ba
xiaozhongde <1065877826@qq.com>
2019-07-19 10:54:31 +0800

    start project

---
+++ src/main/java/com/liyang/lawknowledge/lawknowledge/LawknowledgeApplication.java
@@ -0,0 +1,13 @@
+package com.liyang.lawknowledge.lawknowledge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LawknowledgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LawknowledgeApplication.class, args);
+    }
+
+}
