d6f31566d5d14b0c2c8c709e8d302b1bb8416bd2
xiaozhongde <1065877826@qq.com>
2019-07-19 16:11:33 +0800

    start project

---
+++ src/main/java/com/liyang/lawknowledge/lawknowledge/util/DateUtil.java
@@ -0,0 +1,29 @@
+package com.liyang.lawknowledge.lawknowledge.util;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * @Author: Pan
+ * @Date: 2019/7/4 10:26
+ * @Description:
+ **/
+public class DateUtil {
+
+    public String toName()  {
+        Date date = new Date();
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyyMMddHHmmss");
+        return sdf.format(date);
+    }
+
+    public Date toTime() throws ParseException {
+        Locale locale= Locale.CHINA;
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MMdd-HH:mm:ss",locale);
+        Date date = new Date();
+        String d= sdf.format(date);
+      //  System.out.println(sdf.parse(d));
+        return sdf.parse(d);
+    }
+}
---
+++ src/main/java/com/liyang/lawknowledge/lawknowledge/util/FileUpload.java
@@ -0,0 +1,37 @@
+package com.liyang.lawknowledge.lawknowledge.util;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.util.HashMap;
+
+/**
+ * @Author: Pan
+ * @Date: 2019/7/5 14:24
+ * @Description:
+ **/
+public class FileUpload {
+
+    public HashMap<String,Object> addFile(MultipartFile fileUpload, String filePath){
+        DateUtil dul = new DateUtil();
+        HashMap<String,Object> hm = new HashMap<>();
+        //获取文件名
+        String fileName = fileUpload.getOriginalFilename();
+        //获取文件后缀名
+        String suffixName = fileName.substring(fileName.lastIndexOf("."));
+        //重新生成文件名
+        fileName = dul.toName()+suffixName;
+        //指定本地文件夹存储图片
+        try {
+            //将图片保存到static文件夹里
+            fileUpload.transferTo(new File("D:"+filePath+fileName));
+            hm.put("code",1);
+            hm.put("url",filePath+fileName);
+            return hm;
+        } catch (Exception e) {
+            e.printStackTrace();
+            hm.put("code",0);
+            return  hm;
+        }
+    }
+}
