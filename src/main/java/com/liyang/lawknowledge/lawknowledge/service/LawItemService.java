2d67c82931aca72576449f9aef95140f5c30cb13
xiaozhongde <1065877826@qq.com>
2019-07-21 10:33:59 +0800

    team

--- src/main/java/com/liyang/lawknowledge/lawknowledge/service/LawItemService.java
+++ src/main/java/com/liyang/lawknowledge/lawknowledge/service/LawItemService.java
@@ -11,7 +11,7 @@ import java.util.List;
  * @Date:
  * @Description:
  **/
-public interface LawItemService {
+public interface LawItemService {//...
     List<LawItem> getList(int page);
     LawItem getLawItem(int lawID);
 
