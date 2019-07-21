3b9b09e393fa683edbed3af7657dbeb6e11ab69f
MyName <945023912@qq.com>
2019-07-21 20:25:35 +0800

    add index

---
+++ src/main/java/com/liyang/controller/IndexController.java
@@ -0,0 +1,20 @@
+package com.liyang.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Author: Pan
+ * @Date: 2019/7/21 20:24
+ * @Description:
+ **/
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+    @RequestMapping("index")
+    public String showIndex(){
+        return "index";
+    }
+
+}
