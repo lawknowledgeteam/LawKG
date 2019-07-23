ca07df1194fe60ccb9d28005941a058367c4da00
xiaozhongde <1065877826@qq.com>
2019-07-23 08:51:38 +0800

    Merge branch 'master' of https://github.com/lawknowledgeteam/LawKG
    # Conflicts:
    #	src/main/java/com/liyang/mapper/LawItemTypeMapper.java
    #	src/main/resources/mapping/LawMapping.xml

---
+++ src/main/java/com/liyang/entity/node/LawItemType.java
@@ -0,0 +1,35 @@
+package com.liyang.entity.node;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.neo4j.ogm.annotation.NodeEntity;
+import org.neo4j.ogm.annotation.Property;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.GeneratedValue;
+
+/**
+ * @Author: Pan
+ * @Date: 2019/7/21 20:20
+ * @Description:
+ **/
+@Getter
+@Setter
+@NoArgsConstructor
+@NodeEntity(label = "LawItemType")
+public class LawItemType {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Property(name = "id")
+    private String typeId;
+
+    @Property(name = "law_type_no")
+    private String lawTypeNo;
+
+    @Property(name = "name")
+    private String name;
+
+}
