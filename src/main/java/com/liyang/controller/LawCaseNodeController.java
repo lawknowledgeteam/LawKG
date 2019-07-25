2a5a832e049a75776f5db88ee65e8a1211b5a812
MyName <945023912@qq.com>
2019-07-25 09:55:35 +0800

    add lawCaseNode search

---
+++ src/main/java/com/liyang/controller/LawCaseNodeController.java
@@ -0,0 +1,37 @@
+package com.liyang.controller;
+
+import com.liyang.entity.ObjectNodeRelation;
+import com.liyang.service.LawCaseNodeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @Author: Pan
+ * @Date: 2019/7/25 9:51
+ * @Description:
+ **/
+@RestController
+@RequestMapping("/ LawCaseNode")
+public class LawCaseNodeController {
+    @Autowired
+    LawCaseNodeService lawCaseNodeService;
+
+    @RequestMapping("/searchByName")
+    List<ObjectNodeRelation> searchByName(@RequestParam String name, @RequestParam int page){
+        return lawCaseNodeService.searchByName(name, page);
+    }
+
+    @RequestMapping("/searchSameKind")
+    List<ObjectNodeRelation>  searchSameKind(@RequestParam String name, @RequestParam int page){
+        return lawCaseNodeService.searchSameKind(name, page);
+    }
+
+    @RequestMapping("/searchSameCourt")
+    List<ObjectNodeRelation>  searchSameCourt(@RequestParam String name, @RequestParam int page){
+        return lawCaseNodeService.searchSameCourt(name, page);
+    }
+}
