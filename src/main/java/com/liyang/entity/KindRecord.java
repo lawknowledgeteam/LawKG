b6ea5ca24f1409d431fc9b0aa6afa979f2d9b390
xiaozhongde <1065877826@qq.com>
2019-07-30 08:59:21 +0800

    add record entity

---
+++ src/main/java/com/liyang/entity/KindRecord.java
@@ -0,0 +1,13 @@
+package com.liyang.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class KindRecord {
+    private int kindRecord;
+    private int userID;
+    private String caseKind;
+    private int weight;
+}
---
+++ src/main/java/com/liyang/entity/Record.java
@@ -0,0 +1,13 @@
+package com.liyang.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Record {
+    private int recordID;
+    private int userID;
+    private int caseID;
+    private int weight;
+}
