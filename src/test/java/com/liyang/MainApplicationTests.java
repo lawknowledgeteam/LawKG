5aa43ee3375043eca9e5bcb2837c204e29e9741d
MyName <945023912@qq.com>
2019-11-15 17:13:08 +0800

    memcache git add .!

--- src/test/java/com/liyang/MainApplicationTests.java
+++ src/test/java/com/liyang/MainApplicationTests.java
@@ -1,16 +1,30 @@
 package com.liyang;
 
+import com.whalin.MemCached.MemCachedClient;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MainApplicationTests {
-
+    @Autowired
+    private MemCachedClient memCachedClient;
     @Test
-    public void contextLoads() {
+    public void contextLoads() throws InterruptedException{
+        boolean flag = memCachedClient.set("a", 1);
+        Object a = memCachedClient.get("a");
+        System.out.println(a);
+        memCachedClient.set("b", "2", new Date(3000));
+        Object b = memCachedClient.get("b");
+        System.out.println(b);
+        Thread.sleep(3000);
+        b = memCachedClient.get("b");
+        System.out.println(b);
     }
 
 }
